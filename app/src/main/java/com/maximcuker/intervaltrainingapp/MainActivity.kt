8aab066d208bc2c02bc42fb0a2200f7c2b5863b4
Maximilian6547s <maximilian.s517@gmail.com>
2020-11-25 12:32:30 +0300

    Setting up the UI

--- app/src/main/java/com/maximcuker/intervaltrainingapp/MainActivity.kt
+++ app/src/main/java/com/maximcuker/intervaltrainingapp/MainActivity.kt
@@ -2,10 +2,15 @@ package com.maximcuker.intervaltrainingapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        llStart.setOnClickListener {
+            Toast.makeText(this, "Here we will start exercise", Toast.LENGTH_SHORT).show()
+        }
     }
 }
