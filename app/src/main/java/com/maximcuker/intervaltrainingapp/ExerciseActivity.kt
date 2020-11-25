133bec1b1200ca9595cee577951f9927fb619470
Maximilian6547s <maximilian.s517@gmail.com>
2020-11-25 14:00:25 +0300

    Using countDownTimers to display the seconds remaining

--- app/src/main/java/com/maximcuker/intervaltrainingapp/ExerciseActivity.kt
+++ app/src/main/java/com/maximcuker/intervaltrainingapp/ExerciseActivity.kt
@@ -2,9 +2,16 @@ package com.maximcuker.intervaltrainingapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_exercise.*
 
 class ExerciseActivity : AppCompatActivity() {
+
+    private var restTimer: CountDownTimer? = null
+    private var restProgress = 0
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_exercise)
@@ -17,5 +24,38 @@ class ExerciseActivity : AppCompatActivity() {
         toolbarExerciseActivity.setNavigationOnClickListener {
             onBackPressed()
         }
+
+        setupRestView()
+    }
+
+    override fun onDestroy() {
+        if (restTimer != null) {
+            restTimer?.cancel()
+            restProgress = 0
+        }
+
+        super.onDestroy()
+    }
+    private fun setRestProgressBar() {
+        progressBar.progress = restProgress
+        restTimer = object : CountDownTimer(10000, 1000) {
+            override fun onTick(millisUntilFinished: Long) {
+                restProgress++
+                progressBar.progress = 10 - restProgress
+                tvTimer.text = (10 - restProgress).toString()
+            }
+
+            override fun onFinish() {
+                Toast.makeText(this@ExerciseActivity, "Here now we start the exercise",Toast.LENGTH_LONG).show()
+            }
+        }.start()
+    }
+
+    private fun setupRestView(){
+        if (restTimer != null) {
+            restTimer?.cancel()
+            restProgress = 0
+        }
+        setRestProgressBar()
     }
 }
