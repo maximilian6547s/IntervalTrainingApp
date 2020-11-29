ea3d2f7d7c5ead18a9042918f4df6ce85b2a4ac1
Maximilian6547s <maximilian.s517@gmail.com>
2020-11-29 17:16:00 +0300

    Implement body mass index calculator in metric system

--- app/src/main/java/com/maximcuker/intervaltrainingapp/BMIActivity.kt
+++ app/src/main/java/com/maximcuker/intervaltrainingapp/BMIActivity.kt
@@ -2,8 +2,12 @@ package com.maximcuker.intervaltrainingapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_b_m_i.*
 import kotlinx.android.synthetic.main.activity_exercise.*
+import java.math.BigDecimal
+import java.math.RoundingMode
 
 class BMIActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,5 +20,79 @@ class BMIActivity : AppCompatActivity() {
         toolbarBMIActivity.setNavigationOnClickListener {
             onBackPressed()
         }
+
+        btnCalculateUnits.setOnClickListener {
+            if (validateMetricUnits()) {
+                val height: Float =
+                    etMetricUnitHeight.text.toString().toFloat() / 100 // height in meters
+                val weight: Float = etMetricUnitWeight.text.toString().toFloat()
+
+                val bmi = weight / (height * height)
+                displayBmiResult(bmi)
+            } else {
+                Toast.makeText(this@BMIActivity, "Please enter valid values.", Toast.LENGTH_SHORT)
+                    .show()
+            }
+        }
+    }
+
+    private fun displayBmiResult(bmi: Float) {
+        val bmiLabel: String
+        val bmiDescription: String
+
+        if (bmi.compareTo(15f) <= 0) {
+            bmiLabel = "Very severely underweight"
+            bmiDescription = "Oops! You really need to take better care of yourself! Eat more!"
+        } else if (bmi.compareTo(15f) > 0 && bmi.compareTo(16f) <= 0
+        ) {
+            bmiLabel = "Severely underweight"
+            bmiDescription = "Oops!You really need to take better care of yourself! Eat more!"
+        } else if (bmi.compareTo(16f) > 0 && bmi.compareTo(18.5f) <= 0
+        ) {
+            bmiLabel = "Underweight"
+            bmiDescription = "Oops! You really need to take better care of yourself! Eat more!"
+        } else if (bmi.compareTo(18.5f) > 0 && bmi.compareTo(25f) <= 0
+        ) {
+            bmiLabel = "Normal"
+            bmiDescription = "Congratulations! You are in a good shape!"
+        } else if (bmi.compareTo(25f) > 0 && bmi.compareTo(30f) <= 0
+        ) {
+            bmiLabel = "Overweight"
+            bmiDescription = "Oops! You really need to take care of your yourself! Workout maybe!"
+        } else if (bmi.compareTo(30f) > 0 && bmi.compareTo(35f) <= 0
+        ) {
+            bmiLabel = "Obese Class | (Moderately obese)"
+            bmiDescription = "Oops! You really need to take care of your yourself! Workout maybe!"
+        } else if (bmi.compareTo(35f) > 0 && bmi.compareTo(40f) <= 0) {
+            bmiLabel = "Obese Class || (Severely obese)"
+            bmiDescription = "OMG! You are in a very dangerous condition! Act now!"
+        } else {
+            bmiLabel = "Obese Class ||| (Very Severely obese)"
+            bmiDescription = "OMG! You are in a very dangerous condition! Act now!"
+        }
+
+        tvYourBMI.visibility = View.VISIBLE
+        tvBMIValue.visibility = View.VISIBLE
+        tvBMIType.visibility = View.VISIBLE
+        tvBMIDescription.visibility = View.VISIBLE
+
+        // This is used to round the result value to 2 decimal values after "."
+        val bmiValue = BigDecimal(bmi.toDouble()).setScale(2, RoundingMode.HALF_EVEN).toString()
+
+        tvBMIValue.text = bmiValue // Value is set to TextView
+        tvBMIType.text = bmiLabel // Label is set to TextView
+        tvBMIDescription.text = bmiDescription // Description is set to TextView
+
+    }
+
+    private fun validateMetricUnits(): Boolean {
+        var isValid = true
+        if (etMetricUnitWeight.text.toString().isEmpty()) {
+            isValid = false
+        } else if (etMetricUnitHeight.text.toString().isEmpty()) {
+            isValid = false
+        }
+
+        return isValid
     }
 }
